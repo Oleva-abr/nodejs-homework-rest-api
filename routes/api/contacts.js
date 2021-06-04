2cb9f84a7e1e5a6541bce250a6bbc52ceac11e43
Olena Abramova <olena.v.abramova@gmail.com>
2021-06-04 12:43:08 +0300

    +

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,32 +4,64 @@ const Contacts = require('../../model/index');
 
 router.get('/', async (req, res, next) => {
   try {
-    res.json({ message: 'template message' });
-  } catch {}
+    const contacts = await Contacts.listContacts();
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        contacts,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/:contactId', async (req, res, next) => {
   try {
-    res.json({ message: 'template message' });
-  } catch {}
+    const contact = await Contacts.getContactById(req.params);
+    if (contact) {
+      res.json({
+        status: 'success',
+        code: 200,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        data: 'Not Found',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res, next) => {
   try {
     res.json({ message: 'template message' });
-  } catch {}
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete('/:contactId', async (req, res, next) => {
   try {
     res.json({ message: 'template message' });
-  } catch {}
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.patch('/:contactId', async (req, res, next) => {
   try {
     res.json({ message: 'template message' });
-  } catch {}
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
